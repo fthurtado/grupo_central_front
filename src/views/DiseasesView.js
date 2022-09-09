0679d58ea1477fbb7ba9e2a640eb7b7d44742705
fthurtado <fthurtado@uc.cl>
2022-09-09 15:27:22 -0400

    feat(): icons && show all diseases && left side styles

--- src/views/DiseasesView.js
+++ src/views/DiseasesView.js
@@ -1,10 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, {
+  useEffect,
+  useState,
+} from 'react';
 
 // Styles
 import '../styles/DiseasesStyles.scss';
 
 // Helpers
 import loaddata from '../helpers/loaddata';
+
+// Icons
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+} from "react-icons/ai";
 
 function Diseases() {
 
@@ -19,8 +28,39 @@ function Diseases() {
       })
       .catch((err) => console.log(err));
   }, []);
+
   return (Object.keys(diseases).length > 0) && (
-    <>Hello World!</>
+    <div className="diseases-container">
+      <p className="title">
+        Visor Causas de muerte EEUU
+      </p>
+      <div className="info-container">
+        <div className="left-container">
+          <div className="options">
+            <div className="option">
+              Enfermedad
+            </div>
+            <div className="option">
+              Favoritos
+            </div>
+          </div>
+          <div className="diseases">
+            <input type="text" className="search-bar" placeholder="Nombre enfermedad..." />
+            {Object.keys(diseases).map((name) => (
+              <div className="disease-container">
+                <p>
+                  {name}
+                </p>
+                <AiOutlineHeart />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="right-container">
+          right content
+        </div>
+      </div>
+    </div>
   );
 }
 
